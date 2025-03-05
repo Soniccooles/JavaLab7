12b88cc44c73368fd6c36971cf5f033f2de97112
Soniccooles <andrey.sonic@mail.ru>
2025-03-05 19:27:30 +0500

    Начало модуля 3
    Сервер tomcat apache и сервлет

---
+++ src/main/java/com/example/servlet/MainServlet.java
@@ -0,0 +1,25 @@
+package com.example.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("name", "JSP");
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
